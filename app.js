4c069c3b5c3b2578df587c10fc303321bd84c9a0
Matt Hong <mr.hongaku@berkeley.edu>
2014-06-26 17:54:51 -0700

    adding things

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var ejs = require("ejs");
+var app = express();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('facts.db');
+
+
+///////////////////////////////////////////////////////////////////////////////
+// APP CONFIGURATION                                                         //
+///////////////////////////////////////////////////////////////////////////////
+//configure logging
+app.use(express.logger());
+//make files in static folder publicly accessible
+app.use('/static', express.static(__dirname + '/static'));
+//use ejs for html templates
+app.engine('html', ejs.renderFile);
+
+
+///////////////////////////////////////////////////////////////////////////////
+// APP ROUTES                                                                //
+///////////////////////////////////////////////////////////////////////////////
+//default route
+app.get('/', function(req, res) {
+  res.render('index.html', { });
+});
+
+///////////////////////////////////////////////////////////////////////////////
+// RUN CONFIGURATION                                                         //
+///////////////////////////////////////////////////////////////////////////////
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+    console.log("Yo website is at " + port + " son!");
+});
